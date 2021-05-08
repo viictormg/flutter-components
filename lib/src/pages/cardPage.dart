83c07ad44bfd188c3d5ec5169bcbfab76d77d956
desarrollo54 <desarrollo5@aveonline.co>
2021-05-08 15:58:12 -0500

    segunda verion de card personaliazada

--- lib/src/pages/cardPage.dart
+++ lib/src/pages/cardPage.dart
@@ -10,13 +10,40 @@ class CardPage extends StatelessWidget {
       body: ListView(
         // padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         padding: EdgeInsets.all(10.0),
-        children: [_cartTypeOne()],
+        children: [
+          _cardTypeOne(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTypeTwo(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTypeOne(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTypeTwo(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTypeOne(),
+          SizedBox(
+            height: 30,
+          ),
+          _cardTypeTwo(),
+          SizedBox(
+            height: 30,
+          ),
+        ],
       ),
     ));
   }
 
-  _cartTypeOne() {
+  _cardTypeOne() {
     return Card(
+      elevation: 5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
       child: Column(
         children: [
           ListTile(
@@ -25,7 +52,8 @@ class CardPage extends StatelessWidget {
               color: Colors.red[300],
             ),
             title: Text("titulo perro"),
-            subtitle: Text("este es un texto largo de prueba"),
+            subtitle: Text(
+                "este es un texto largo de prueba este es un texto largo de prueba este es un texto largo de prueba este es un texto largo de prueba"),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
@@ -38,4 +66,39 @@ class CardPage extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget _cardTypeTwo() {
+  return (Container(
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          boxShadow: <BoxShadow>[
+            BoxShadow(
+                color: Colors.black26,
+                blurRadius: 5,
+                spreadRadius: 2.0,
+                offset: Offset(2.0, 10.0))
+          ]),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            children: <Widget>[
+              FadeInImage(
+                fadeInDuration: Duration(milliseconds: 200),
+                placeholder: AssetImage('assets/loading.gif'),
+                image: NetworkImage(
+                    'https://pm1.narvii.com/6201/52c4ef1ffe82a66f3f57577527dffd4330dfb4d4_hq.jpg'),
+                height: 300,
+                fit: BoxFit.cover,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+                child: Text("prueba"),
+              )
+            ],
+          ),
+        ),
+      )));
 }
